a81ec67759574cd446770f7e905e1d4c6d92d454
karko13 <145532713+karko13@users.noreply.github.com>
2023-11-14 18:23:28 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PripremaVjezba04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upisite dva broja jedan cijeli drugi decimalni ");
+            string a = Console.ReadLine();
+            int x = Convert.ToInt32(a.Split(' ')[0]);
+            decimal y = Convert.ToDecimal(a.Split(' ')[1]);
+            Console.WriteLine("prvi broj " + x + "\ndrugi broj " + y);
+            Console.ReadKey();
+        }
+    }
+}
